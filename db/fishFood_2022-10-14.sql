8fd934de89eedd0903996499035b3e5ac56faabf
cr-walton <cxw507@gmail.com>
2022-10-14 15:22:36 +0100

    updated database structure with correct sql file

--- db/fishFood_2022-10-14.sql
+++ db/fishFood_2022-10-14.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.5.5-10.8.3-MariaDB-1:10.8.3+maria~jammy)
 # Database: fishFood
-# Generation Time: 2022-10-14 13:31:53 +0000
+# Generation Time: 2022-10-14 14:21:02 +0000
 # ************************************************************
 
 
@@ -18,6 +18,63 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table ingredients
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `ingredients`;
+
+CREATE TABLE `ingredients` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+# Dump of table recipes
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `recipes`;
+
+CREATE TABLE `recipes` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL DEFAULT '',
+  `duration` int(10) NOT NULL,
+  `cookTime` int(10) DEFAULT NULL,
+  `prepTime` int(10) DEFAULT NULL,
+  `instructions` varchar(10000) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+# Dump of table recipes_ingredients
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `recipes_ingredients`;
+
+CREATE TABLE `recipes_ingredients` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `recipeId` int(10) NOT NULL,
+  `ingredientId` int(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+# Dump of table users
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 
 
 # Dump of table users_recipes
