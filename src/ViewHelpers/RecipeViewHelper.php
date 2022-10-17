2e1622a05fdc8dc393dce014683257892eebf4d4
mikeycodingstuff <mikeyinguk@gmail.com>
2022-10-17 16:54:33 +0100

    wip added more to viewhelper

--- src/ViewHelpers/RecipeViewHelper.php
+++ src/ViewHelpers/RecipeViewHelper.php
@@ -8,11 +8,13 @@ class RecipeViewhelper
     {
         $result = '';
         foreach ($userRecipes as $userRecipe) {
-            $result .= '<div class="col recipe m-3">';
+            $result .= '<div class="col recipe m-3 p-0">';
             $result .= '<div class="d-flex align-items-center m-2 justify-content-between">';
             $result .= '<h2>' . $userRecipe->getName() . '</h2>';
-            $result .= '<h3>' . $userRecipe->getDuration() . '</h3></div>';
-            $result .= '</div>';
+            $result .= '<h3>' . $userRecipe->getDuration() . ' minutes</h3></div>';
+            $result .= '<div class="recipe-content">';
+            $result .= '<p class="m-0">' . $userRecipe->getInstructions() . '</p>';
+            $result .= '</div></div>';
         }
         return $result;
     }
