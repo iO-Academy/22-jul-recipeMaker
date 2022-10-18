4ee4ee04d1ce8dc71a995fdf14b8e97d415d30a8
mikeycodingstuff <mikeyinguk@gmail.com>
2022-10-18 10:18:11 +0100

    wip more work on viewhelper. changed fonts import to include bold styling

--- src/ViewHelpers/RecipeViewHelper.php
+++ src/ViewHelpers/RecipeViewHelper.php
@@ -10,10 +10,16 @@ class RecipeViewhelper
         foreach ($userRecipes as $userRecipe) {
             $result .= '<div class="col-12 col-lg-3 recipe m-3 p-0">';
             $result .= '<div class="d-flex align-items-center m-2 justify-content-between">';
-            $result .= '<h2>' . $userRecipe->getName() . '</h2>';
-            $result .= '<h3>' . $userRecipe->getDuration() . ' minutes</h3></div>';
+            $result .= '<h2 class="my-auto recipe-name">' . $userRecipe->getName() . '</h2>';
+            $result .= '<div class="d-flex"><h3 class="my-auto recipe-duration mx-1">';
+            $result .= $userRecipe->getDuration() . ' mins</h3>';
+            $result .= '<button class="btn-success recipe-button">+</button></div></div>';
             $result .= '<div class="recipe-content">';
-            $result .= '<p class="m-0">' . $userRecipe->getInstructions() . '</p>';
+            $result .= '<div class="d-flex justify-content-between">';
+            $result .= '<p class="m-1">Cooking time: ' . $userRecipe->getCookTime() . ' mins</p>';
+            $result .= '<p class="m-1">Preparation time: ' . $userRecipe->getPrepTime() . ' mins</p></div>';
+            $result .= '<p class="font-weight-bold m-1">Instructions: </p>';
+            $result .= '<p class="m-0 p-1">' . $userRecipe->getInstructions() . '</p>';
             $result .= '</div></div>';
         }
         return $result;
