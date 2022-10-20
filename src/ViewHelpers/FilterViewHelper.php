7028624525bbda725d0e6396c05d0e32d4475a48
mikeycodingstuff <mikeyinguk@gmail.com>
2022-10-20 15:57:25 +0100

    added filterviewhelpertest. fixed bug in js with redirecting with no ingredients selected.

--- src/ViewHelpers/FilterViewHelper.php
+++ src/ViewHelpers/FilterViewHelper.php
@@ -7,14 +7,18 @@ class FilterViewHelper
     public static function displayIngredientsChecklist(array $userIngredients)
     {
         $result = '';
-        foreach ($userIngredients as $userIngredient) {
-            $result .= '<input type="checkbox" id="' . $userIngredient->getIngredientId() . '" ';
-            $result .= 'name="' . $userIngredient->getIngredientId() . '" ';
-            $result .= 'value="' . $userIngredient->getName() . '"';
-            $result .= 'class="filter-option">';
-            $result .= '<label for="' . $userIngredient->getIngredientId();
-            $result .= '">&nbsp;' . $userIngredient->getName() . '';
-            $result .= '</label><br>';
+        if ($userIngredients !== []) {
+            foreach ($userIngredients as $userIngredient) {
+                $result .= '<input type="checkbox" id="' . $userIngredient->getIngredientId() . '" ';
+                $result .= 'name="' . $userIngredient->getIngredientId() . '" ';
+                $result .= 'value="' . $userIngredient->getName() . '"';
+                $result .= 'class="filter-option">';
+                $result .= '<label for="' . $userIngredient->getIngredientId();
+                $result .= '">&nbsp;' . $userIngredient->getName() . '';
+                $result .= '</label><br>';
+            }
+        } else {
+            $result = '<p>You do not have any ingredients</p>';
         }
         return $result;
     }
